5613acb1a30159196218cdb3f13c758f0e31ed20
ctgu_zhw_home <ctgu_zhw_home@sina.com>
2016-12-02 00:28:48 +0800

    上传更新代码

---
+++ UnityProject_zhw/Assets/script/camera.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+var speed : int = 50;
+var newObject : Transform;
+function Update () {
+
+	var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+	var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+	transform.Translate(x,0,z);
+
+	if (Input.GetButtonDown("Fire1")) 
+	{
+		var n: Transform = Instantiate(newObject,transform.position,transform.rotation);
+		var fwd:Vector3 = transform.TransformDirection(Vector3.forward);
+		n.GetComponent.<Rigidbody>().AddForce(fwd*20055);
+	}
+
+
+}
