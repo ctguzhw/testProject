ca48a7b1557802498e8673156ab9309d4bbdfeda
ctgu_zhw_home <ctgu_zhw_home@sina.com>
2016-12-02 22:22:44 +0800

    修改功能

--- UnityProject_zhw/Assets/script/camera.js
+++ UnityProject_zhw/Assets/script/camera.js
@@ -3,20 +3,66 @@
 function Start () {
 
 }
-var speed : int = 50;
+var speed : int = 20;
+var rotateSpeed : int = 40;
+var heightSpeed :int = 10;
 var newObject : Transform;
+
+
 function Update () {
 
-	var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-	var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+// 平移摄像机
+	var x :float = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+	var z :float= Input.GetAxis("Vertical") * Time.deltaTime * speed;
 	transform.Translate(x,0,z);
 
+// 摄像机高度调节
+	if (Input.GetKey(KeyCode.H))
+	{
+		transform.Translate(0,(1) * heightSpeed * Time.deltaTime,0);
+	}
+	if (Input.GetKey(KeyCode.N))
+	{
+		transform.Translate(0,(-1) * heightSpeed * Time.deltaTime,0);
+	}
+
+
+// Y轴旋转
+	if (Input.GetKey(KeyCode.Q))
+	{
+		transform.Rotate(0,(-1) * rotateSpeed * Time.deltaTime,0,Space.Self);
+
+	}
+	if (Input.GetKey(KeyCode.E))
+	{
+		transform.Rotate(0,(1) * rotateSpeed * Time.deltaTime,0,Space.Self);
+
+	}
+
+
+// X轴旋转
+	if (Input.GetKey(KeyCode.Z))
+	{
+		transform.Rotate((-1) * rotateSpeed * Time.deltaTime,0,0,Space.Self);
+
+	}
+	if (Input.GetKey(KeyCode.C))
+	{
+		transform.Rotate((1) * rotateSpeed * Time.deltaTime,0,0,Space.Self);
+
+	}
+
+
+// 射击
 	if (Input.GetButtonDown("Fire1")) 
 	{
 		var n: Transform = Instantiate(newObject,transform.position,transform.rotation);
 		var fwd:Vector3 = transform.TransformDirection(Vector3.forward);
 		n.GetComponent.<Rigidbody>().AddForce(fwd*20055);
 	}
+
+
+
 
 
 }
