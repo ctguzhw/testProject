e7316f70771f2b812cf2f50fe161dce7cc798356
zhw_comany <zhw_comany@sina.com>
2016-12-06 19:08:56 +0800

    添加文本测试

--- Assets/script/jiancha.js
+++ Assets/script/jiancha.js
@@ -13,6 +13,7 @@ function Update () {
 		xm = gameObject.Find("Camera").GetComponent(sheji).xmshu++;
 		sd = gameObject.Find("Camera").GetComponent(sheji).sdshu;
 		print("射弹数" + xm);
+		gameObject.Find("Canvas/ziti").GetComponent(UI.Text).text = "射弹数" + xm;
 		Destroy(gameObject);
 	}	
 };
