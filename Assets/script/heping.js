9d4d9ec8f0fe8f232e58e21ec1817185cdeca0e9
zhw_comany <zhw_comany@sina.com>
2016-12-08 10:43:49 +0800

    添加新场景

--- Assets/script/heping.js
+++ Assets/script/heping.js
@@ -9,5 +9,12 @@ function Update () {
 }
 
 function OnCollisionEnter(){
+	
+	gameObject.Find("Canvas/zt").GetComponent(UI.Text).text = "如果世界失去了和平，世界将会怎样";
+	gameObject.Find("Point light").GetComponent(Light).enabled = false;
+
+
+	//销毁物体
 	Destroy(gameObject);
+
 }
